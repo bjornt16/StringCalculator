a3256699ca75db15fa2353515312aaa89df591ff
bjornt16 <bjornt16@ru.is>
2017-10-15 14:56:10 +0000

    minor adjustment

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -6,28 +6,34 @@ import java.util.List;
 public class Calculator {
 	
 	public static int add(String text) {
+		
 		int[] numbers;
 		int sum = 0;
 		int temp = 0;
 		String neg = "";
 		String del = "";
+		
 		if(text.isEmpty()) {
+			
 			return 0;
+			
 		} else {
+			
 			if(text.length() > 1 && text.substring(0, 2).equals("//")) {
 				del = "|" + text.substring(2, text.indexOf("\n"));
 				text = text.substring(text.indexOf("\n")+1, text.length());
 			}
 			
-			System.out.println(text);
-			
 			String[] textNumbers = text.split("\n|,"+del);
 			numbers = new int[textNumbers.length];
+			
 			for(int i = 0; i < textNumbers.length; i++){
+				
 				temp = Integer.parseInt(textNumbers[i]);
 				if(temp > 1000) {
 					continue;
 				}
+				
 				numbers[i] = temp;
 				sum += numbers[i];
 				if(numbers[i] < 0) {
