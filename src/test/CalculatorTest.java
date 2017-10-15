b0a0c6c2c4ceedb5894ff60ffdc1582e3fa07c98
bjornt16 <bjornt16@ru.is>
2017-10-15 14:02:07 +0000

    TDD Cycle 4 - added test for random number of arguments, no refactoring needed

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -3,6 +3,7 @@ package test;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import java.util.Random;
 import main.Calculator;
 
 public class CalculatorTest {
@@ -20,6 +21,23 @@ public class CalculatorTest {
 	@Test
 	public void testtwoNumber() {
 		assertEquals(3, Calculator.add("1,2"));
+	}
+	
+	@Test
+	public void testRandomArgumentLength() {
+		Random random = new Random();
+		int loop = random.nextInt(100);
+		int sum = 0;
+		String text = "";
+		
+		for(int i = 0; i < loop; i++) {
+			sum += 1;
+			text += i == 0 ? "1" : ",1"; 
+		}
+		
+		System.out.print(sum +" "+ text);
+		
+		assertEquals(sum, Calculator.add(text));
 	}
 
 }
