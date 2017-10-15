ff692cbf2d428dcb51a2f4e07274fb9e787009f4
bjornt16 <bjornt16@ru.is>
2017-10-15 13:47:48 +0000

    TDD Cycle: 2 - added testonenumber and some code in calculator.add

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,13 +1,25 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
 	
 	public static int add(String text) {
+		int[] numbers;
+		int sum = 0;
 		if(text.isEmpty()) {
 			return 0;
+		} else {
+			String[] textNumbers = text.split(",");
+			numbers = new int[textNumbers.length];
+			for(int i = 0; i < textNumbers.length; i++){
+				numbers[i] = Integer.parseInt(textNumbers[i]);
+				sum += numbers[i];
+			}
 		}
 		
-		return -1;
+		return sum;
 	}
 
 }
